bca4c9c73997f33480cd361093f927f9a8049cfd
Willochs316 <willochs316@gmail.com>
2023-04-07 17:57:32 +0100

    implement function to register user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,9 @@ const bcrypt = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 
+// @desc Register new user
+// @route POST /api/users
+// @access Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -43,6 +46,9 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc Authenticate a user
+// @route POST /api/users/login
+// @access Public
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
@@ -66,6 +72,9 @@ const loginUser = asyncHandler(async (req, res) => {
   });
 });
 
+// @desc Get user data
+// @route GET /api/users/me
+// @access Public
 const getMe = asyncHandler(async (req, res) => {
   const { _id, name, email } = await User.findById(req.user.id);
   res.json({
